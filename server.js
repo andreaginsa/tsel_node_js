50488de3087a01a9b806e271adcd9d7a552e688d
Andre Aginsa <Andre Aginsa>
2016-10-18 17:00:22 +0700

    initial commit

--- server.js
+++ server.js
@@ -1,11 +1,34 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
+var http = require("http");
+var fs = require('fs');
 
 app.get('/', function (req, res) {
     res.send("Hello dev, am a local route nobody knows me yet!");
 });
 
-var server = app.listen(port, function () {
-    console.log('node server is just fine! and running on port - ' + port);
-});
+//var server = app.listen(port, function () {
+//    console.log('node server is just fine! and running on port - ' + port);
+//});
+
+http.createServer(function(req,res) {
+    var kode = 0;
+    var file = "";
+
+    if(req.url == "/") {
+        kode = 200;
+        file = "index.html";
+    } else if(req.url == "/contact") {
+        kode = 200;
+        file = "contact.html";
+    } else {
+        kode = 404;
+        file = "404.html";
+    }
+
+    res.writeHead(kode, {"Content-Type" : "text/html"});
+    fs.createReadStream('./template/'+file).pipe(res);
+}).listen(port);
+
+console.log("Server is running...");
